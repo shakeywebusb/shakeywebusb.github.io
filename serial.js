b7de7993bb9432380809137580fab83e50a6c95f
JP Liew <jpliew@yahoo.com>
2018-11-17 22:06:12 +1100

    added Shakey WebUSB official VID,PID, fixed serial dynamic interface selection

--- serial.js
+++ serial.js
@@ -1,7 +1,4 @@
 var serial = {};
-var interfaceNumber=2;		// original interface number of WebUSB Arduino demo
-var endpointIn=5;			// original in endpoint ID of WebUSB Arduino demo
-var endpointOut=4;			// original out endpoint ID of WebUSB Arduino demo
 
 (function() {
   'use strict';
@@ -14,12 +11,7 @@ var endpointOut=4;			// original out endpoint ID of WebUSB Arduino demo
 
   serial.requestPort = function() {
     const filters = [
-      { 'vendorId': 0x2341, 'productId': 0x8036 },
-      { 'vendorId': 0x2341, 'productId': 0x8037 },
-      { 'vendorId': 0x2341, 'productId': 0x804d },
-      { 'vendorId': 0x2341, 'productId': 0x804e },
-      { 'vendorId': 0x2341, 'productId': 0x804f },
-      { 'vendorId': 0x2341, 'productId': 0x8050 },
+      { 'vendorId': 0x310A, 'productId': 0x1003 },
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
@@ -28,11 +20,14 @@ var endpointOut=4;			// original out endpoint ID of WebUSB Arduino demo
 
   serial.Port = function(device) {
     this.device_ = device;
+    this.interfaceNumber_ = 2;  // original interface number of WebUSB Arduino demo
+    this.endpointIn_ = 5;       // original in endpoint ID of WebUSB Arduino demo
+    this.endpointOut_ = 4;      // original out endpoint ID of WebUSB Arduino demo
   };
 
   serial.Port.prototype.connect = function() {
     let readLoop = () => {
-      this.device_.transferIn(endpointIn, 64).then(result => {
+      this.device_.transferIn(this.endpointIn_, 64).then(result => {
         this.onReceive(result.data);
         readLoop();
       }, error => {
@@ -46,40 +41,32 @@ var endpointOut=4;			// original out endpoint ID of WebUSB Arduino demo
             return this.device_.selectConfiguration(1);
           }
         })
-		.then(()=> {
-			this.device_.configuration.interfaces.forEach(function(element) {
-				var found=false;
-				console.log(element.interfaceNumber);
-				
-				element.alternates.forEach(function(elementalt) {
-					if (elementalt.interfaceClass==0xff) {
-						console.log("Found!");
-						console.log("interfaceClass=%s",elementalt.interfaceClass);
-						found=true;
-						interfaceNumber=element.interfaceNumber;
-						
-						elementalt.endpoints.forEach(function(elementendpoint) {
-							if (elementendpoint.direction=="out") {
-								endpointOut=elementendpoint.endpointNumber;
-								console.log("out endpoint ID=%s", endpointOut);
-							}
-							if (elementendpoint.direction=="in") {
-								endpointIn=elementendpoint.endpointNumber;
-								console.log("in endpoint ID=%s", endpointIn);
-							}
-						})
-					}
-				})
-			})
-		})
-        .then(() => this.device_.claimInterface(interfaceNumber))
-        .then(() => this.device_.selectAlternateInterface(interfaceNumber, 0))
+        .then(() => {
+          var configurationInterfaces = this.device_.configuration.interfaces;
+          configurationInterfaces.forEach(element => {
+            element.alternates.forEach(elementalt => {
+              if (elementalt.interfaceClass==0xff) {
+                this.interfaceNumber_ = element.interfaceNumber;
+                elementalt.endpoints.forEach(elementendpoint => {
+                  if (elementendpoint.direction == "out") {
+                    this.endpointOut_ = elementendpoint.endpointNumber;
+                  }
+                  if (elementendpoint.direction=="in") {
+                    this.endpointIn_ =elementendpoint.endpointNumber;
+                  }
+                })
+              }
+            })
+          })
+        })
+        .then(() => this.device_.claimInterface(this.interfaceNumber_))
+        .then(() => this.device_.selectAlternateInterface(this.interfaceNumber_, 0))
         .then(() => this.device_.controlTransferOut({
             'requestType': 'class',
             'recipient': 'interface',
             'request': 0x22,
             'value': 0x01,
-            'index': interfaceNumber}))
+            'index': this.interfaceNumber_}))
         .then(() => {
           readLoop();
         });
@@ -91,11 +78,11 @@ var endpointOut=4;			// original out endpoint ID of WebUSB Arduino demo
             'recipient': 'interface',
             'request': 0x22,
             'value': 0x00,
-            'index': interfaceNumber})
+            'index': this.interfaceNumber_})
         .then(() => this.device_.close());
   };
 
   serial.Port.prototype.send = function(data) {
-    return this.device_.transferOut(endpointOut, data);
+    return this.device_.transferOut(this.endpointOut_, data);
   };
 })();
